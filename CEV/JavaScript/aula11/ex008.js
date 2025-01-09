69aabe068295a728128e9c785ec08e046f912b73
joaquim7zz <joaquimdss.7z@gmail.com>
2025-01-09 11:13:44 -0300

    nova atividade CEV

---
+++ CEV/JavaScript/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}`)
+if (vel > 60) {
+    console.log('Você foi multado!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ CEV/JavaScript/aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+if (pais == 'Brasil') {
+    console.log('Brasileiro')
+} else {
+    console.log('Não é brasileiro')
+}
