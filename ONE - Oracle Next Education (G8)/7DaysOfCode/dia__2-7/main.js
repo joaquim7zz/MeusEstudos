dc2a134c7131f1ca0833b0230597d8f6549000ea
Joaquim de Sena <162745299+joaquim7zz@users.noreply.github.com>
2025-02-27 22:34:35 -0300

    7daysofcode

---
+++ ONE - Oracle Next Education (G8)/7DaysOfCode/dia__2-7/main.js
@@ -0,0 +1,4 @@
+let nome = prompt("Qual é seu nome?")
+let idade = prompt('Qual é sua idade?')
+let ling = prompt('Qual linguagem vocês está estudando?')
+document.write(`Olá ${nome}, você está apredendo ${ling}`)
