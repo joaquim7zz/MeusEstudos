487422bb3f345f1a441ade08675eca56713e270b
Joaquim de Sena <162745299+joaquim7zz@users.noreply.github.com>
2025-03-03 23:15:32 -0300

    deletei 7daysofcode

--- ONE - Oracle Next Education (G8)/7DaysOfCode/dia__2-7/main.js
+++
@@ -1,4 +0,0 @@
-let nome = prompt("Qual é seu nome?")
-let idade = prompt('Qual é sua idade?')
-let ling = prompt('Qual linguagem vocês está estudando?')
-document.write(`Olá ${nome}, você está apredendo ${ling}`)
