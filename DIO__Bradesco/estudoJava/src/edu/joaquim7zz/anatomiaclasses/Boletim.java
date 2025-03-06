359b7c3138ca8270537bf547d954590769d29503
Joaquim de Sena <162745299+joaquim7zz@users.noreply.github.com>
2025-03-05 23:13:53 -0300

    att intellij

--- DIO__Bradesco/estudoJava/src/edu/joaquim7zz/anatomiaclasses/Boletim.java
+++ DIO__Bradesco/estudoJava/src/edu/joaquim7zz/anatomiaclasses/Boletim.java
@@ -3,12 +3,13 @@ package edu.joaquim7zz.anatomiaclasses;
 public class Boletim {
     public static void main(String[] args) {
     
-    int mediaFinal = 6;
-    if (mediaFinal < 6) 
+    int mediaFinal = 7;
+    if (mediaFinal < 6) {
         System.out.println("Reprovado");
-    else if (mediaFinal == 6) 
+    } else if (mediaFinal == 6) {
         System.out.println("Prova minerva");
-    else 
+    } else {
         System.out.println("Aprovado");
+    }
     }
 }
